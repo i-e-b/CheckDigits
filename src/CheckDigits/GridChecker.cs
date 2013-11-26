b07eabf4571a71e6be96c4469a7c5b2d172652ee
Iain Ballard <iain.ballard@7digital.com>
2013-11-26 16:30:19 +0000

    Cleanup

--- src/CheckDigits/GridChecker.cs
+++ src/CheckDigits/GridChecker.cs
@@ -4,12 +4,17 @@ namespace CheckDigits
 {
 	public class GridChecker : IValidate, IProduceChecksum
 	{
+		const string lookup = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		/// <summary>
+		/// Returns true for valid GRids, false otherwise
+		/// </summary>
 		public bool IsValid(string grid)
 		{
 			var input = grid.ToUpperInvariant();
 			var skip = (input.StartsWith("GRID:")) ? (5) : (0);
 
-			var a = input.Skip(skip).Where(Iso7064.AlphanumericCharSet.Contains).ToArray();
+			var a = input.Skip(skip).Where(lookup.Contains).ToArray();
 			var b = string.Concat(a.Take(17));
 			var c = string.Concat(a.Last());
 
@@ -27,18 +32,14 @@ namespace CheckDigits
 			int p = 36;
 			var ip = input.ToArray();
 
-			int sj = 0;
 			foreach (var a in ip)
 			{
-				var na = Iso7064.AlphanumericCharSet.IndexOf(a);
-				var s = (p % 37) + (na);
-				sj = (s == 36) ? (36) : (s % 36);
-
-				p = 2 * sj;
+				var s = p + (lookup.IndexOf(a));
+				var sj = (s == 36) ? (36) : (s % 36);
+				p = (2 * sj) % 37;
 			}
-			p = (2 * sj) % 37;
 
-			return string.Concat(Iso7064.AlphanumericCharSet[37 - p]);
+			return string.Concat(lookup[37 - p]);
 		}
 	}
 }
