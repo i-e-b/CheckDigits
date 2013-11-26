8757054ba51d70bb4d66aef418573a6de56d0262
Iain Ballard <iain.ballard@7digital.com>
2013-11-26 14:45:17 +0000

    Project stub

---
+++ src/CheckDigits.Tests/GRiDs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CheckDigits.Tests
+{
+	[TestFixture]
+	public class GRiDs
+	{
+
+		[Test]
+		public void stub_test ()
+		{
+			Assert.Pass();
+		}
+	}
+}
